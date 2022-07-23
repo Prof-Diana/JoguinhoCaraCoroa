566f0fb9fd53606b5922dad393ce87efcd6ce0e6
Professora Diana <94633813+Prof-Diana@users.noreply.github.com>
2022-07-23 20:28:20 -0300

    Create Program.java

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.CaraCoroa;
+
+	public class Program {
+	    
+	   public static void main(String[] args) {
+
+		 CaraCoroa jogo = new CaraCoroa();
+       
+	   }
+
+	}
